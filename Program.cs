933d44fb1aef59d759e67a0f7709419f44845e4e
hosseinnoori72 <noori.hossein72@gmail.com>
2021-12-24 23:01:06 +0330

    ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+{
+     
+}
+namespace project
+{
+    public class program
+    {
+        static void main()
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+    
+}
